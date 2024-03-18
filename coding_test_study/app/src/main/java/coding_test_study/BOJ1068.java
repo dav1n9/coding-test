80c735cdd7ec2c5d6a400520ae07f7d3d16c8f5f
Davin <85967571+dav1n9@users.noreply.github.com>
2024-03-18 11:27:12 +0900

    BFS 이용한 풀이로 수정

--- coding_test_study/app/src/main/java/coding_test_study/BOJ1068.java
+++ coding_test_study/app/src/main/java/coding_test_study/BOJ1068.java
@@ -25,28 +25,31 @@ public class BOJ1068 {
         }
 
         int removeNode = Integer.parseInt(br.readLine());
-        
-        // removeNode 의 모든 자손에서 리프노드를 찾을 수 없도록 수정한다.
         if (removeNode == root) System.out.println(0);  // removeNode가 root 이면 0 출력.
         else {
-            update(removeNode);
-
+            // removeNode 의 부모 노드에서 removeNode를 제외하고 탐색한다.
             int result = 0;
-            for(int i = 0; i < tree.size(); i++) {
-                // removeNode 의 부모 노드에서 removeNode를 삭제한다.
-                if(tree.get(i).contains(removeNode)) tree.get(i).remove(tree.get(i).indexOf(removeNode));
-                // 자손이 없는 노드를 찾는다.
-                if(tree.get(i).isEmpty()) result++;
+
+            Queue<Integer> q = new LinkedList<>();
+            boolean[] isVisited = new boolean[n];
+            q.add(root);
+
+            while(!q.isEmpty()) {
+                int cur = q.poll();
+
+                int nodeSize = tree.get(cur).size();
+                if(tree.get(cur).contains(removeNode)) nodeSize--;
+                if(nodeSize == 0 && cur != removeNode) result++;
+                
+                isVisited[cur] = true;
+
+                for(int a : tree.get(cur)) {
+                    if(!isVisited[a] && a != removeNode) q.add(a); 
+                }
             }
             System.out.println(result);
         }
 
         br.close();
-    }
-
-    public static void update(int node) {
-        if(tree.get(node).size() >= 2) update(tree.get(node).get(1));
-        if(tree.get(node).size() >= 1) update(tree.get(node).get(0));
-        if(tree.get(node).isEmpty()) tree.get(node).add(-2);    
     }
 }
